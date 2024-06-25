ff84c4914b7ac3ef907fe874adced6fef50940f0
FranPiccinato <107905840+FranPiccinato@users.noreply.github.com>
2024-06-25 01:15:43 -0600

    Pantallas de registro y autentiación

--- website/auth.py
+++ website/auth.py
@@ -2,10 +2,18 @@ from flask import Blueprint, render_template
 
 auth = Blueprint('auth', __name__)
 
-@auth.route('/login')
-def login():
-    return render_template("login.html")
+@auth.route('/registrar-usuarios')
+def registrarUsuarios():
+    return render_template("admin_usuarios.html")
 
-@auth.route('/registro')
-def sign_up():
-    return render_template("registro.html")
+@auth.route('/registrar-parqueos')
+def registrarParqueos():
+    return render_template("admin_parqueos.html")
+
+@auth.route('/registrar-vehiculos')
+def registrarVehiculos():
+    return render_template("admin_vehiculos.html")
+
+@auth.route('/cambio')
+def cambioLogin():
+    return render_template("cambioContra.html")
