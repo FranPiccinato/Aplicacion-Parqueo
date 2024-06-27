a1500da902a2d900c00323b22e35f187306265b3
FranPiccinato <107905840+FranPiccinato@users.noreply.github.com>
2024-06-26 22:06:13 -0600

    Errores y funciones

--- website/auth.py
+++ website/auth.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, render_template, request, flash, redirect, url_for
-from werkzeug.security import generate_password_hash, check_password_hash
-from flask_login import login_user, login_required, logout_user, current_user
+from werkzeug.security import generate_password_hash
+from flask_login import login_required, logout_user, current_user
 from .models import Usuario, Parqueo, Vehiculo
 from . import db
 
@@ -12,23 +12,8 @@ def login():
         correo = request.form.get('correo') # Obtiene los valores del form
         contra = request.form.get('contrasena')
 
-        usuario = Usuario.query.filter_by(correo=correo).first() #Obtiene el primer usuario donde el correo sea igual al correo ingresado 
-        if usuario: # Si existe
-            if usuario.contra == contra or check_password_hash(usuario.contra, contra):
-                if usuario.contra == 'Ulacit123': # Si la contraseña es Ulacit123 redirige a la pantalla de cambio de contraseña
-                    return redirect(url_for('auth.cambioLogin'))
-                else:
-                    login_user(usuario, remember=True) # Recuerda el usuario que se encuentra ingresado
-                    if usuario.rol == 'Estudiante' or usuario.rol == 'Administrativo':
-                        flash('En progreso', category='success')
-                    elif usuario.rol == 'Administrador': # Si ingresa un Admin redirige a la pantalla del admin
-                        return redirect(url_for('auth.registrarUsuarios'))
-                    elif usuario.rol == 'Oficial': 
-                        flash('En progreso', category='success')
-            else:
-                flash('Contraseña incorrecta',  category='error')  
-        else:
-            flash('Usuario incorrecta',  category='error')  
+        return Usuario.loginUsuario(correo, contra)
+
             
     return render_template("/login.html") # Carga login.html
 
@@ -41,19 +26,9 @@ def registrarUsuarios():
         nCarne = request.form.get('noUsuario')
         fecha = request.form.get('fechaUsuario')
         rol = request.form.get('rol')
-     
+    
+        return Usuario.registrarUsuario(nombre, id, correo, nCarne, fecha, rol)
 
-        if len(nombre) < 2:
-            flash('El nombre debe de contener más de 2 carácteres.',  category='error')
-        elif len(correo) < 4:
-            flash('El correo debe de contener más de 4 carácteres.',  category='error')
-            pass
-        else:
-            nuevoUsuario = Usuario(id=id, nombre=nombre, correo=correo, nCarne=nCarne, fecha= fecha, rol=rol, contra = 'Ulacit123') # Crea un nuevo usuario y pone la contraseña por default
-            db.session.add(nuevoUsuario) # Se agrega a la base de datos
-            db.session.commit()  # Hace un commit a la base de datos para guardar los datos
-            flash('Cuenta creada con éxito.',  category='success')
-        
     return render_template("admin_usuarios.html") # Carga admin_usuarios.html
 
 @auth.route('/registrar-parqueos', methods=['GET', 'POST'])
@@ -63,15 +38,9 @@ def registrarParqueos():
         capacidadES = request.form.get('capacidadES')
         capacidadMotos = request.form.get('capacidadMoto')
         capacidadLey = request.form.get('capacidadLey')
-     
 
-        if len(nombre) < 2:
-            flash('El nombre debe de contener más de 2 carácteres.',  category='error')
-        else:
-            nuevoParqueo = Parqueo(nombre=nombre, capacidadES=capacidadES , capacidadMotos=capacidadMotos, capacidadLey= capacidadLey) # Crea un nuevo parqueo
-            db.session.add(nuevoParqueo)  # Se agrega a la base de datos
-            db.session.commit()  # Hace un commit a la base de datos para guardar los datos
-            flash('Parqueo creado con éxito.',  category='success')
+        return Parqueo.registrarParqueo(nombre, capacidadES, capacidadMotos, capacidadLey)
+    
     return render_template("admin_parqueos.html") # Carga admin_parqueos.html
 
 @auth.route('/registrar-vehiculos', methods=['GET', 'POST'])
@@ -85,19 +54,7 @@ def registrarVehiculos():
         placa = request.form.get('noPlaca')
         espacio = request.form.get('espacioLey')
 
-        ley = False
-        if espacio == 'on': 
-            ley = True
-        else:
-            ley = False
-
-        if len(marca) < 2:
-            flash('La marca debe de contener más de 2 carácteres.',  category='error')
-        else:
-            nuevoVehiculo = Vehiculo(marca=marca, tipo=tipo , color=color, dueno= dueno, placa = placa, espacio=ley, id_usuario = dueno) # Crea un nuevo vehículo
-            db.session.add(nuevoVehiculo) # Se agrega a la base de datos
-            db.session.commit() # Hace un commit a la base de datos para guardar los datos
-            flash('Vehículo creado con éxito.',  category='success')
+        return Vehiculo.registrarVehiculo(marca, tipo, color, dueno, placa, espacio)
       
 
     return render_template("admin_vehiculos.html",  usuario= Usuario.query.all()) # Carga admin_vehículos.html y pasa la variable usuario para utilizarla en el archivo .html
@@ -108,15 +65,17 @@ def cambioLogin():
         nuevaContra = request.form.get('cambioContrasena') # Obtiene los valores del form
         contra = request.form.get('confirmarContrasena')
         id = current_user.id
-        if nuevaContra == contra: # Verifica que las contraseñas sean iguales
-            usuario = Usuario.query.filter_by(id=id).first() # Obtiene el usuario 
-            usuario.contra = generate_password_hash(nuevaContra, method='pbkdf2:sha256') # Se hace un UPDATE de la contrasena y se utiliza un método para encriptar la contrasena
-            db.session.commit() # Hace un commit a la base de datos para guardar el cambio
-            return redirect(url_for('auth.login')) # Redirige a la pantalla de login 
+        if len(nuevaContra) > 4:
+            if nuevaContra == contra: # Verifica que las contraseñas sean iguales
+                usuario = Usuario.query.filter_by(id=id).first() # Obtiene el usuario 
+                usuario.contra = generate_password_hash(nuevaContra, method='pbkdf2:sha256') # Se hace un UPDATE de la contrasena y se utiliza un método para encriptar la contrasena
+                db.session.commit() # Hace un commit a la base de datos para guardar el cambio
+                return redirect(url_for('auth.login')) # Redirige a la pantalla de login 
+            else:
+                flash('Las contraseñas no son iguales', category='error')
         else:
-            flash('Las contraseñas no son iguales', category='error')
-
-            
+            flash('La contraseña debe de ser mayor a 4 caracteres', category='error')
+        
     return render_template("cambioContra.html") # Carga cambioContra.html
 
 @auth.route('/logout') # Cierra sesión del usuario actual
--- website/models.py
+++ website/models.py
@@ -1,5 +1,7 @@
 from . import db
-from flask_login import UserMixin # Métodos para manejar la gestión de sesiones y autenticación 
+from flask import flash, redirect, url_for
+from werkzeug.security import check_password_hash
+from flask_login import UserMixin, login_user # Métodos para manejar la gestión de sesiones y autenticación 
 
 class Usuario(db.Model, UserMixin): # Modelo SQLAlchemy para le estructura de la tabla
     id = db.Column(db.Integer, primary_key=True)
@@ -11,12 +13,62 @@ class Usuario(db.Model, UserMixin): # Modelo SQLAlchemy para le estructura de la
     contra = db.Column(db.String(150))
     vehiculo = db.relationship('Vehiculo')
 
+    def loginUsuario(correo, contra):
+        usuario = Usuario.query.filter_by(correo=correo).first() #Obtiene el primer usuario donde el correo sea igual al correo ingresado 
+        if usuario: # Si existe
+            if usuario.contra == contra or check_password_hash(usuario.contra, contra):
+                if usuario.contra == 'Ulacit123': # Si la contraseña es Ulacit123 redirige a la pantalla de cambio de contraseña
+                    login_user(usuario, remember=True)
+                    return redirect(url_for('auth.cambioLogin'))
+                else:
+                    login_user(usuario, remember=True) # Recuerda el usuario que se encuentra ingresado
+                    if usuario.rol == 'Estudiante' or usuario.rol == 'Administrativo':
+                        flash('En progreso', category='success')
+                    elif usuario.rol == 'Administrador': # Si ingresa un Admin redirige a la pantalla del admin
+                        return redirect(url_for('auth.registrarUsuarios'))
+                    elif usuario.rol == 'Oficial': 
+                        flash('En progreso', category='success')
+            else:
+                flash('Contraseña incorrecta',  category='error')  
+        else:
+            flash('Correo incorrecto',  category='error')  
+        return redirect(url_for('auth.login'))
+    
+    def registrarUsuario(nombre, id, correo, nCarne, fecha, rol):
+        
+        if len(nombre) < 2:
+            flash('El nombre debe de contener más de 2 carácteres.',  category='error')
+        elif len(correo) < 4:
+            flash('El correo debe de contener más de 4 carácteres.',  category='error')
+            pass
+        else:
+            nuevoUsuario = Usuario(id=id, nombre=nombre, correo=correo, nCarne=nCarne, fecha= fecha, rol=rol, contra = 'Ulacit123') # Crea un nuevo usuario y pone la contraseña por default
+            db.session.add(nuevoUsuario) # Se agrega a la base de datos
+            db.session.commit()  # Hace un commit a la base de datos para guardar los datos
+            flash('Cuenta creada con éxito.',  category='success')
+        return redirect(url_for('auth.registrarUsuarios'))
+
+
+
 class Parqueo(db.Model): # Modelo SQLAlchemy para le estructura de la tabla
     id = db.Column(db.Integer, primary_key=True)
     nombre = db.Column(db.String(150), unique=True)
     capacidadES = db.Column(db.Integer)
     capacidadMotos = db.Column(db.Integer)
     capacidadLey = db.Column(db.Integer)
+
+    def registrarParqueo(nombre, capacidadES, capacidadMotos, capacidadLey):
+        if len(nombre) < 2:
+            flash('El nombre debe de contener más de 2 carácteres.',  category='error')
+        else:
+            nuevoParqueo = Parqueo(nombre=nombre, capacidadES=capacidadES , capacidadMotos=capacidadMotos, capacidadLey= capacidadLey) # Crea un nuevo parqueo
+            db.session.add(nuevoParqueo)  # Se agrega a la base de datos
+            db.session.commit()  # Hace un commit a la base de datos para guardar los datos
+            flash('Parqueo creado con éxito.',  category='success')
+    
+        return redirect(url_for('auth.registrarParqueos'))
+
+
 
 class Vehiculo(db.Model): # Modelo SQLAlchemy para le estructura de la tabla
     id = db.Column(db.Integer, primary_key=True)
@@ -27,3 +79,20 @@ class Vehiculo(db.Model): # Modelo SQLAlchemy para le estructura de la tabla
     placa = db.Column(db.String(100))
     espacio = db.Column(db.Boolean)
     id_usuario = db.Column(db.Integer, db.ForeignKey('usuario.id'))
+
+    def registrarVehiculo(marca, tipo, color, dueno, placa, espacio):
+        ley = False
+        if espacio == 'on': 
+            ley = True
+        else:
+            ley = False
+
+        if len(marca) < 2:
+            flash('La marca debe de contener más de 2 carácteres.',  category='error')
+        else:
+            nuevoVehiculo = Vehiculo(marca=marca, tipo=tipo , color=color, dueno= dueno, placa = placa, espacio=ley, id_usuario = dueno) # Crea un nuevo vehículo
+            db.session.add(nuevoVehiculo) # Se agrega a la base de datos
+            db.session.commit() # Hace un commit a la base de datos para guardar los datos
+            flash('Vehículo creado con éxito.',  category='success')
+        
+        return redirect(url_for('auth.registrarVehiculos'))
